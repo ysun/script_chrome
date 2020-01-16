f34804d9207177072c48ebc6ca99017f3a68c2e7
Yi Sun <syqust@gmail.com>
2020-01-16 00:36:32 -0600

    Force cpu frequency to max.
    Signed-off-by: Yi Sun <syqust@gmail.com>

--- enable_cores.sh
+++ enable_cores.sh
@@ -4,4 +4,6 @@ set -x
 for((i=0; i<4; i++)) {
 	echo 1 > /sys/devices/system/cpu/cpu${i}/online
 	echo performance > /sys/devices/system/cpu/cpu${i}/cpufreq/scaling_governor
+
+	cat /sys/devices/system/cpu/cpu${i}/cpufreq/scaling_max_freq > /sys/devices/system/cpu/cpu${i}/cpufreq/scaling_min_freq
 }
