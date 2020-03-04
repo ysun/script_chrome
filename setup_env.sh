627c6d4a5a9a1c993969e021ff05024d61e02036
Eason Sun <syqust@gmail.com>
2020-03-03 22:55:08 -0800

    Copy top_cpu and top_gpu to /usr/local/bin
    Signed-off-by: Eason Sun <syqust@gmail.com>

--- setup_env.sh
+++ setup_env.sh
@@ -11,3 +11,6 @@ iptables -P INPUT ACCEPT
 iptables -P OUTPUT ACCEPT
 iptables -P FORWARD ACCEPT
 echo "Deleted rules and forced all default policies accepted"
+
+cp top_gpu.sh /usr/local/bin/
+cp top_cpu.sh /usr/local/bin/
